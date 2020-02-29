0694f419d2a537e19d6f03da507a41000a4bdfbc
kabalpt <47484768+kabalpt@users.noreply.github.com>
2020-02-29 11:25:15 +0000

    Add multilanguage fallback (#4)

--- createPagesFromDocuments.js
+++ createPagesFromDocuments.js
@@ -4,13 +4,21 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _extends2 = require('babel-runtime/helpers/extends');
-
-var _extends3 = _interopRequireDefault(_extends2);
-
 var _slicedToArray2 = require('babel-runtime/helpers/slicedToArray');
 
 var _slicedToArray3 = _interopRequireDefault(_slicedToArray2);
+
+var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
+
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+
+var _extends5 = require('babel-runtime/helpers/extends');
+
+var _extends6 = _interopRequireDefault(_extends5);
+
+var _keys = require('babel-runtime/core-js/object/keys');
+
+var _keys2 = _interopRequireDefault(_keys);
 
 var _lodash = require('lodash');
 
@@ -28,56 +36,77 @@ var createPagesFromDocuments = function createPagesFromDocuments(_ref) {
 
   var globalContext = typeof onCreatePages === 'function' ? onCreatePages({ documents: documents }) || {} : {};
 
+  var defaultLang = (0, _keys2.default)(langs).reduce(function (prev, current) {
+    return langs[current].default ? current : prev;
+  }, null);
+
+  var documentCollection = documents.reduce(function (prev, current) {
+    return (0, _extends6.default)({}, prev, (0, _defineProperty3.default)({}, current.type, (0, _extends6.default)({}, (0, _lodash.get)(prev, '' + current.type, {}), (0, _defineProperty3.default)({}, current.uid, (0, _extends6.default)({}, (0, _lodash.get)(prev, current.type + '.' + current.uid, {}), (0, _defineProperty3.default)({}, current.lang, current))))));
+  }, {});
+
   layoutNames.map(function (type) {
-    var results = documents.filter(function (document) {
-      var _document$type$split = document.type.split(layoutsKey),
-          _document$type$split2 = (0, _slicedToArray3.default)(_document$type$split, 2),
-          _document$type$split3 = _document$type$split2[1],
-          documentSlug = _document$type$split3 === undefined ? '' : _document$type$split3;
+    var documentTypes = (0, _keys2.default)(documentCollection).filter(function (documentType) {
+      var _documentType$split = documentType.split(layoutsKey),
+          _documentType$split2 = (0, _slicedToArray3.default)(_documentType$split, 2),
+          _documentType$split2$ = _documentType$split2[1],
+          documentSlug = _documentType$split2$ === undefined ? '' : _documentType$split2$;
 
       return documentSlug.replace('-', '_') === type;
     });
+
     var _layouts$type = layouts[type],
         component = _layouts$type.component,
         onCreatePage = _layouts$type.onCreatePage;
 
 
-    if (!results || !component) {
+    if (_lodash.isEmpty[documentTypes] || !component) {
       return;
     }
 
-    results.forEach(function (document) {
-      var id = document.id,
-          lang = document.lang,
-          uid = document.uid;
+    documentTypes.forEach(function (documentType) {
+      var documents = documentCollection[documentType];
 
-      var locale = (0, _lodash.get)(langs, '.' + lang);
-      var path = (locale && !locale.default ? locale.path : '') + '/' + (uid || id);
-      var context = globalContext;
+      (0, _keys2.default)(documents).map(function (uid) {
+        (0, _keys2.default)(langs).map(function (lang) {
+          var document = documents[uid][lang] || documents[uid][defaultLang];
 
-      if (typeof onCreatePageDefault === 'function') {
-        context = onCreatePageDefault((0, _extends3.default)({ document: document, documents: documents }, context)) || context;
-      }
+          document.lang = lang;
+          document.uid = uid;
 
-      if (typeof onCreatePage === 'function') {
-        context = onCreatePage((0, _extends3.default)({ document: document, documents: documents }, context)) || context;
-      }
-
-      context = (0, _extends3.default)({ document: document }, context);
-
-      if (context.path) {
-        var _context = context,
-            changedPath = _context.path;
+          var id = document.id;
 
 
-        delete context.path;
-        path = changedPath;
-      }
+          var locale = (0, _lodash.get)(langs, lang);
 
-      createPage({
-        component: component,
-        context: context,
-        path: path
+          var path = (locale && !locale.default ? locale.path : '') + '/' + (uid || id);
+          var context = globalContext;
+
+          if (typeof onCreatePageDefault === 'function') {
+            context = onCreatePageDefault((0, _extends6.default)({ document: document, documents: documents }, context)) || context;
+          }
+
+          if (typeof onCreatePage === 'function') {
+            context = onCreatePage((0, _extends6.default)({ document: document, documents: documents }, context)) || context;
+            context.onCreatePage = onCreatePage;
+          }
+
+          context = (0, _extends6.default)({ document: document }, context);
+
+          if (context.path) {
+            var _context = context,
+                changedPath = _context.path;
+
+
+            delete context.path;
+            path = changedPath;
+          }
+
+          createPage({
+            component: component,
+            context: context,
+            path: path
+          });
+        });
       });
     });
   });
--- src/createPagesFromDocuments.js
+++ src/createPagesFromDocuments.js
@@ -1,4 +1,4 @@
-import { get } from 'lodash';
+import { get, isEmpty } from 'lodash';
 
 const createPagesFromDocuments = ({
   createPage,
@@ -12,45 +12,76 @@ const createPagesFromDocuments = ({
 }) => {
   const globalContext = typeof onCreatePages === 'function' ? onCreatePages({ documents }) || {} : {};
 
+  const defaultLang = Object.keys(langs).reduce((prev, current) => (langs[current].default ? current : prev), null);
+
+  const documentCollection = documents.reduce(
+    (prev, current) => ({
+      ...prev,
+      [current.type]: {
+        ...get(prev, `${current.type}`, {}),
+        [current.uid]: {
+          ...get(prev, `${current.type}.${current.uid}`, {}),
+          [current.lang]: current
+        }
+      }
+    }),
+    {}
+  );
+
   layoutNames.map(type => {
-    const results = documents.filter(document => {
-      const [, documentSlug = ''] = document.type.split(layoutsKey);
+    const documentTypes = Object.keys(documentCollection).filter(documentType => {
+      const [, documentSlug = ''] = documentType.split(layoutsKey);
 
       return documentSlug.replace('-', '_') === type;
     });
+
     const { component, onCreatePage } = layouts[type];
 
-    if (!results || !component) {
+    if (isEmpty[documentTypes] || !component) {
       return;
     }
 
-    results.forEach(document => {
-      const { id, lang, uid } = document;
-      const locale = get(langs, `.${lang}`);
-      let path = `${locale && !locale.default ? locale.path : ''}/${uid || id}`;
-      let context = globalContext;
+    documentTypes.forEach(documentType => {
+      const documents = documentCollection[documentType];
 
-      if (typeof onCreatePageDefault === 'function') {
-        context = onCreatePageDefault({ document, documents, ...context }) || context;
-      }
+      Object.keys(documents).map(uid => {
+        Object.keys(langs).map(lang => {
+          const document = documents[uid][lang] || documents[uid][defaultLang];
 
-      if (typeof onCreatePage === 'function') {
-        context = onCreatePage({ document, documents, ...context }) || context;
-      }
+          document.lang = lang;
+          document.uid = uid;
 
-      context = { document, ...context };
+          const { id } = document;
 
-      if (context.path) {
-        const { path: changedPath } = context;
+          const locale = get(langs, lang);
 
-        delete context.path;
-        path = changedPath;
-      }
+          let path = `${locale && !locale.default ? locale.path : ''}/${uid || id}`;
+          let context = globalContext;
 
-      createPage({
-        component,
-        context,
-        path
+          if (typeof onCreatePageDefault === 'function') {
+            context = onCreatePageDefault({ document, documents, ...context }) || context;
+          }
+
+          if (typeof onCreatePage === 'function') {
+            context = onCreatePage({ document, documents, ...context }) || context;
+            context.onCreatePage = onCreatePage;
+          }
+
+          context = { document, ...context };
+
+          if (context.path) {
+            const { path: changedPath } = context;
+
+            delete context.path;
+            path = changedPath;
+          }
+
+          createPage({
+            component,
+            context,
+            path
+          });
+        });
       });
     });
   });
